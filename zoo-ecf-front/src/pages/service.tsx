942277f1347e2ab497921e7c72933840e2baf4da
ayoubyao1210@gmail.com <ayoubyao1210@gmail.com>
2024-07-22 10:47:06 +0400

    ** fix cardelement

--- zoo-ecf-front/src/pages/service.tsx
+++ zoo-ecf-front/src/pages/service.tsx
@@ -2,7 +2,6 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import HeaderComponent from "./components/headerComponent";
-import CardElement from "./components/cardElement";
 import FooterComponent from "./components/FooterComponent";
 import Link from "next/link";
 import { ServiceService } from "@/services/serviceService";
@@ -45,12 +44,20 @@ const Restauration: NextPage<Props> = ({}) => {
           <br />
           <div className="grid grid-cols-3 gap-1">
           {services.map((services) => (
-            <CardElement
-              key={services.service_id}
-              title={services.nom}
-              sourceImage={"services/" + services.imagePrincipal}
-              lien={"/servicesDetails?title=" + services.nom + "&idservice=" + services.service_id}
-            />
+            <Link
+            key={services.service_id}
+            href={"/servicesDetails?title=" + services.nom + "&idservice=" + services.service_id}
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {services.nom}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              En savoir plus
+            </p>
+            <br />
+            <img src={"services/" + services.imagePrincipal} alt="" />
+          </Link>
           ))}
             
           </div>
